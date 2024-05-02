d65f25f1d828a7f6f18454e38e5247d292a4773b
AmarinoLama <amarinolama@gmail.com>
2024-05-02 12:47:43 +0200

    feat (AgedBrie) : add methods

--- src/main/java/com/gildedrose/AgedBrie.java
+++ src/main/java/com/gildedrose/AgedBrie.java
@@ -1,4 +1,26 @@
 package com.gildedrose;
 
-public class AgedBrie {
+public class AgedBrie extends Item {
+    AgedBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateItem() {
+        updateSellIn();
+        updateQuality();
+    }
+
+    void updateQuality() {
+        if (sellIn > 0) {
+            quality = quality + 1;
+        } else {
+            quality = quality + 2;
+        }
+    }
+
+    void updateSellIn() {
+        if (sellIn > 0) {
+            sellIn = sellIn - 1;
+        }
+    }
 }
