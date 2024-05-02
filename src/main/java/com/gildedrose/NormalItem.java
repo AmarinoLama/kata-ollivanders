a841bbd0c3d9bc3b5ad7ec172802cfdb0b878493
AmarinoLama <amarinolama@gmail.com>
2024-05-02 12:09:13 +0200

    feat (NormalItem) : create class and add methods

---
+++ src/main/java/com/gildedrose/NormalItem.java
@@ -0,0 +1,27 @@
+package com.gildedrose;
+
+public class NormalItem extends Item {
+    public NormalItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateItem() {
+        updateSellIn();
+        updateQuality();
+    }
+
+    private void updateQuality() {
+        if (quality > 0 && sellIn > 0) {
+            quality = quality - 1;
+        } else {
+            quality = quality - 2;
+        }
+
+    }
+
+    private void updateSellIn() {
+        if (sellIn > 0) {
+            sellIn = sellIn - 1;
+        }
+    }
+}
