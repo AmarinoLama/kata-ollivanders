d067efba47a99e74762e9f9ffe00779ea9982983
AmarinoLama <amarinolama@gmail.com>
2024-05-02 12:43:02 +0200

    refactor (GildedRose) : change name of the method updateQuality

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -6,7 +6,7 @@ class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
-    public void updateQuality() {
+    public void updateItem() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
