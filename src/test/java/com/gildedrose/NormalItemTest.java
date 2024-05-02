b0f9985c8285c1ddc4742d8c88e646795eca3aaf
AmarinoLama <amarinolama@gmail.com>
2024-05-02 12:25:03 +0200

    test (NormalItemTest) : add test updateItem

--- src/test/java/com/gildedrose/NormalItemTest.java
+++ src/test/java/com/gildedrose/NormalItemTest.java
@@ -6,13 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NormalItemTest {
 
-
-
     @Test
     public void testCreateNormalItem() {
         NormalItem normalItem = new NormalItem("Elixir of gus", 10, 20);
         assertEquals("Elixir of gus", normalItem.name);
         assertEquals(10, normalItem.sellIn);
         assertEquals(20, normalItem.quality);
+    }
+
+    @Test
+    public void testUpdateItem() {
+        NormalItem normalItem = new NormalItem("Elixir of gus", 10, 20);
+        normalItem.updateItem();
+        assertEquals(19, normalItem.quality);
+        assertEquals(9, normalItem.sellIn);
     }
 }
