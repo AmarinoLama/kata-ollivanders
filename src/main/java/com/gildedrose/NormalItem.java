dbb17f27d47d726b56b69e77c5b2afd49243b87c
AmarinoLama <amarinolama@gmail.com>
2024-05-05 23:05:49 +0200

    refactor (NormalItem) : change visibility of the constructor

--- src/main/java/com/gildedrose/NormalItem.java
+++ src/main/java/com/gildedrose/NormalItem.java
@@ -2,7 +2,7 @@ package com.gildedrose;
 
 public class NormalItem extends Item {
 
-    public NormalItem(String name, int sellIn, int quality) {
+    NormalItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
