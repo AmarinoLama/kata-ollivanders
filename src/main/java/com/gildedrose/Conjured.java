3f1df074a0141f9e4eaefecc9f0b655e5f02ee1e
AmarinoLama <amarinolama@gmail.com>
2024-05-05 23:06:10 +0200

    feat (Conjured) : add methods

--- src/main/java/com/gildedrose/Conjured.java
+++ src/main/java/com/gildedrose/Conjured.java
@@ -1,4 +1,27 @@
 package com.gildedrose;
 
-public class Conjured {
+public class Conjured extends Item {
+
+    Conjured(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateItem() {
+        updateSellIn();
+        updateQuality();
+    }
+
+    void updateQuality() {
+        if (quality > 0 && sellIn > 0) {
+            quality = quality - 2;
+        } else {
+            quality = quality - 4;
+        }
+    }
+
+    void updateSellIn() {
+        if (sellIn > 0) {
+            sellIn = sellIn - 1;
+        }
+    }
 }
